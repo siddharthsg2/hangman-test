1df3752cf67da0dbaf1d5fa5cd1190331d400872
Siddharth <siddharthguptasg2@gmail.com>
2021-05-18 15:09:12 +0530

    com 2

--- hangman.py
+++ hangman.py
@@ -92,7 +92,7 @@ def hangman(secret_word):
             get_guessed_word(secret_word, letters_guessed)))
         letters_guessed.append(letter)
         print("")
-
+# comment 2
 #comment
 # Load the list of words into the variable wordlist
 # So that it can be accessed from anywhere in the program
